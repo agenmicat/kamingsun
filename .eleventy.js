f432dbf3175c90bfd900663a1e09733383d0bb47
agenmicat <113223785+agenmicat@users.noreply.github.com>
2026-02-01 19:29:28 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 module.exports = function (eleventyConfig) {
 
+  eleventyConfig.addPassthroughCopy("assets");
+
   eleventyConfig.addCollection("posts", function (collectionApi) {
-    return collectionApi.getFilteredByGlob("posts/*.md")
-	reverse();
+    return collectionApi.getFilteredByGlob("posts/*.md").reverse();
   });
-	eleventyConfig.addPassthroughCopy("assets");
+
   return {
     dir: {
       input: ".",
